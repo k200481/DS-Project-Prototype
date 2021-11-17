1fe1e90d123613beb9de62bd666ee98119023d92
msaarimkh@gmail.com <msaarimkh@gmail.com>
2021-11-17 23:51:49 +0500

    Temporary commit Testing how much can be done with this system

--- DS Project Prototype/Main.cpp
+++ DS Project Prototype/Main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
+#include <sstream>
 #include "ProcessManager.h"
+
+int fun(int num)
+{
+	if (num <= 1)
+		return 1;
+	return fun(num - 1) + fun(num - 2);
+}
 
 int main(void)
 {
 	class EchoMessage : public ProcessManager::Message
 	{
 	public:
-		EchoMessage(int typeID, int senderID, const char* msg)
+		EchoMessage(int senderID, const char* msg)
 			:
-			ProcessManager::Message(typeID, senderID),
+			ProcessManager::Message(1, senderID),
 			msg(msg)
 		{}
 
@@ -20,19 +28,71 @@ int main(void)
 		const char* msg;
 	};
 
-	ProcessManager::MessageHandler mh;
-	mh.insert({ mh.size() + 1, [](const std::unique_ptr<ProcessManager::Message>& msg) { std::cout << "Message ID: " << msg->GetID() << std::endl; } });
-	mh.insert({ mh.size() + 1, [](const std::unique_ptr<ProcessManager::Message>& msg) { std::cout << ((EchoMessage*)msg.get())->GetMessageString() << std::endl; }});
+	class PocessableMessage : public ProcessManager::Message
+	{
+	public:
+		PocessableMessage(int senderID, int num)
+			:
+			ProcessManager::Message(2, senderID),
+			num(num)
+		{}
 
+		int GetNum() const
+		{
+			return num;
+		}
+
+	private:
+		const int num;
+	};
+
+	class SleepMessage : public ProcessManager::Message
+	{
+	public:
+		SleepMessage(int senderID)
+			:
+			ProcessManager::Message(3, senderID)
+		{}
+	};
+
+	ProcessManager::MessageHandler mh;
+	
+	mh.insert({ mh.size() + 1, [](const std::unique_ptr<ProcessManager::Message>& msg) 
+		{ 
+			EchoMessage new_msg(1, ((EchoMessage*)msg.get())->GetMessageString());
+			std::ostringstream oss;
+			oss << new_msg.GetSenderID() << ": " << new_msg.GetMessageString();
+			std::cout << oss.str();
+		}
+	});
+	mh.insert({ mh.size() + 1, [](const std::unique_ptr<ProcessManager::Message>& msg)
+		{
+			int num = ((PocessableMessage*)msg.get())->GetNum();
+			std::ostringstream oss;
+			oss << fun(num) << std::endl;
+			std::cout << oss.str();
+		}
+	});
+	mh.insert({ mh.size() + 1, [](const std::unique_ptr<ProcessManager::Message>& msg) 
+		{
+			Sleep(100);
+		} 
+	});
 
 	ProcessManager pm(std::move(mh));
 	pm.AddProcess();
 	pm.AddProcess();
 	pm.AddProcess();
 	pm.AddProcess();
+	pm.AddProcess();
+	pm.AddProcess();
+	pm.AddProcess();
+	pm.AddProcess();
 
-	pm.BroadcastMessage(std::make_unique<EchoMessage>(2, 0, "Hi"));
-	pm.BroadcastMessage(std::make_unique<EchoMessage>(2, 0, "Bye"));
+	pm.BroadcastMessage(std::make_unique<EchoMessage>(0, "Hi\n"));
+	pm.BroadcastMessage(std::make_unique<EchoMessage>(0, "Bye\n"));
+	pm.BroadcastMessage(std::make_unique<PocessableMessage>(0, 20));
+//	pm.BroadcastMessage(std::make_unique<SleepMessage>(0));
 
 	return 0;
 }
